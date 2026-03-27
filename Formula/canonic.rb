da03c9a4f2d19cd137b68e446a438201fc45023b
João Lopes <me@joaolopes.dev.br>
2026-03-27 17:15:17 +0000

    update canonic to v0.1.4

--- Formula/canonic.rb
+++ Formula/canonic.rb
@@ -1,16 +1,16 @@
 class Canonic < Formula
   desc "AI behavioral rules, composed and distributed"
   homepage "https://github.com/parletre/canonic"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/parletre/canonic/releases/download/v0.1.3/canonic-macos-arm64"
-      sha256 "bb2bc14ed44859d91906cc77af68308670a34ad664c802d6ad1ab00b2646f272"
+      url "https://github.com/parletre/canonic/releases/download/v0.1.4/canonic-macos-arm64"
+      sha256 "7189694648d15bb47107411826e87396d73a7b0556cee183b2e28de248118d9f"
     else
-      url "https://github.com/parletre/canonic/releases/download/v0.1.3/canonic-macos-x64"
-      sha256 "1c08e9f0963f1c99cd433ff492e931e05dc89c3f327550af31cbfe0ef2925e8d"
+      url "https://github.com/parletre/canonic/releases/download/v0.1.4/canonic-macos-x64"
+      sha256 "3a4a5185f6e7d19da86461730c946c7aa3d02804f419ce58f742720243460e87"
     end
   end
 
