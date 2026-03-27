97b6051cabc6ab067f1040deda616de82d84216b
João Lopes <me@joaolopes.dev.br>
2026-03-27 13:44:21 +0000

    update canonic to v0.1.0

--- Formula/canonic.rb
+++ Formula/canonic.rb
@@ -6,17 +6,17 @@ class Canonic < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/parletre/canonic/releases/download/v#{version}/canonic-macos-arm64"
-      sha256 "PLACEHOLDER"
+      url "https://github.com/parletre/canonic/releases/download/v0.1.0/canonic-macos-arm64"
+      sha256 "7112b81551d2cbc267e47a0ecbdc9117a1eea8da790516b6d2d9eef4836d7b68"
     else
-      url "https://github.com/parletre/canonic/releases/download/v#{version}/canonic-macos-x64"
-      sha256 "PLACEHOLDER"
+      url "https://github.com/parletre/canonic/releases/download/v0.1.0/canonic-macos-x64"
+      sha256 "34852497d765741564d7d069c06147a5ba30252c12815ee97d876ae1f7a5a7f8"
     end
   end
 
   on_linux do
-    url "https://github.com/parletre/canonic/releases/download/v#{version}/canonic-linux-x64"
-    sha256 "PLACEHOLDER"
+    url "https://github.com/parletre/canonic/releases/download/v0.1.0/canonic-linux-x64"
+    sha256 "34753bc43a73c0964de6fdfc6f9e3c4f39ab765bf26ede21901bb20dcb47abf1"
   end
 
   def install
